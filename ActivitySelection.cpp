f8dedb1b9befd7b718400d588dedc879271d6d1d
sakshambassi <sakshambassi@gmail.com>
2018-05-30 11:43:52 +0530

    ActivitySelectionPush

---
+++ ActivitySelection.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printMaxActivities(int s[],int f[], int n)
+{
+	int lastTime=f[0];
+	for(int i=0; i<n; i++)
+	{
+		if(i==0)
+		{
+			cout<<i<<" ";
+		}
+		else
+		{
+			if(s[i]>=lastTime)
+			{
+				cout<<i<<" ";
+				lastTime=f[i];
+			}
+		}
+	}
+}
+
+int main()
+{
+    int s[] =  {1, 3, 0, 5, 8, 5};
+    int f[] =  {2, 4, 6, 7, 9, 9};
+    int n = sizeof(s)/sizeof(s[0]);
+    printMaxActivities(s, f, n);
+    return 0;
+}
